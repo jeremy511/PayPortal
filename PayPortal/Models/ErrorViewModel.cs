7ccb7ed6ff026c673157ff72b74fa4bb8bb0b439
GeanluisGL <geanluislorenzoro@gmail.com>
2023-03-09 23:15:31 -0400

    inity1

---
+++ PayPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PayPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
